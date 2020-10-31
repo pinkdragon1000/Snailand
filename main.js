fa9a9543d8998b3b74d050b1b424850318d1ccae
Sita Robinson <sita.robinson@gmail.com>
2020-10-30 23:19:05 -0400

    adding files

---
+++ main.js
@@ -0,0 +1,33 @@
+function characterSelectAndStartGame() {
+  window.location.href = "./game.html";
+}
+
+function leftArrowPressed() {
+  var element = document.getElementById("character");
+  element.style.left = parseInt(element.style.left) - 10 + "px";
+}
+
+function rightArrowPressed() {
+  var element = document.getElementById("character");
+  element.style.left = parseInt(element.style.left) + 10 + "px";
+}
+
+function spaceBarPressed() {
+  var element = document.getElementById("character");
+  element.style.left = parseInt(element.style.left) + 10 + "px";
+  element.style.bottom = parseInt(element.style.bottom) + 30 + "px";
+}
+
+function moveSelection(event) {
+  switch (event.keyCode) {
+    case 37:
+      leftArrowPressed();
+      break;
+    case 39:
+      rightArrowPressed();
+      break;
+    case 32:
+      spaceBarPressed();
+      break;
+  }
+}
